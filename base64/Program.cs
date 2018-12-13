f01252be27fbb3f5c31935867f632e04a0f5c348
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-13 08:52:24 +0100

    ill codice trasforma la stringa in codice ASCII che a sua volta viene trasformato in base 64

---
+++ base64/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace base64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainText = "Man";
+            string base64EncodedData;
+            var PlainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            for(int i = 0; i < PlainTextBytes.Length; i++)
+            {
+                Console.WriteLine(PlainTextBytes[i]);
+                Console.WriteLine(Convert.ToString(PlainTextBytes[i], 2));
+
+            }
+            base64EncodedData = Convert.ToBase64String(PlainTextBytes);
+            Console.WriteLine(base64EncodedData);
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            plainText = System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+            Console.WriteLine(plainText);
+            Console.ReadLine();
+        }
+    }
+}
